ca70a6ded57478ea0b40f8ae32513a0c186670b9
Adil Ashurov <ashurovadil96@gmail.com>
2025-08-07 00:40:20 +0400

    Добавлены versel.json

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,6 @@ import { handle } from 'hono/vercel'
 import app from '../src/index.js' // путь до твоего `Hono` приложения
 
 export const config = {
-    runtime: 'nodejs22.x' // обязательно с версией
+    runtime: 'nodejs' // обязательно с версией
   }
 export default handle(app)
