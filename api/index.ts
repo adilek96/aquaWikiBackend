36c47afbaab2854dcc83729af43712e6a41f39c1
Adil Ashurov <ashurovadil96@gmail.com>
2025-08-07 00:33:13 +0400

    Добавлены versel.json

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,6 @@ import { handle } from 'hono/vercel'
 import app from '../src/index.js' // путь до твоего `Hono` приложения
 
 export const config = {
-  runtime: "edge", // или 'nodejs' если используешь Prisma
-}
-
+    runtime: 'nodejs22.x' // обязательно с версией
+  }
 export default handle(app)
