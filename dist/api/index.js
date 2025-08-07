6ecb05d4172561ac832b73335cebdf60478f3d30
Adil Ashurov <ashurovadil96@gmail.com>
2025-08-07 10:07:48 +0400

    еще раз

--- dist/api/index.js
+++ dist/api/index.js
@@ -1,68 +1,70 @@
-import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
-import { PrismaClient } from '@prisma/client';
-import { createMiddleware } from 'hono/factory';
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
+import { createMiddleware } from "hono/factory";
 // Импорт роутеров для категорий
-import postCategory from '../src/routes/categories/category/post.category.js';
-import getCategories from '../src/routes/categories/get.categories.js';
-import patchCategory from '../src/routes/categories/category/patch.category.js';
-import deleteCategory from '../src/routes/categories/category/delete.category.js';
+import postCategory from "../src/routes/categories/category/post.category.js";
+import getCategories from "../src/routes/categories/get.categories.js";
+import patchCategory from "../src/routes/categories/category/patch.category.js";
+import deleteCategory from "../src/routes/categories/category/delete.category.js";
 // Импорт роутеров для подкатегорий
-import postSubCategory from '../src/routes/subcategories/subcategory/post.subcategory.js';
-import getSubCategories from '../src/routes/subcategories/get.subcategories.js';
-import patchSubCategory from '../src/routes/subcategories/subcategory/patch.subcategory.js';
-import deleteSubCategory from '../src/routes/subcategories/subcategory/delete.subcategory.js';
+import postSubCategory from "../src/routes/subcategories/subcategory/post.subcategory.js";
+import getSubCategories from "../src/routes/subcategories/get.subcategories.js";
+import patchSubCategory from "../src/routes/subcategories/subcategory/patch.subcategory.js";
+import deleteSubCategory from "../src/routes/subcategories/subcategory/delete.subcategory.js";
 // Импорт роутеров для статей
-import postArticle from '../src/routes/articles/article/post.article.js';
-import getArticles from '../src/routes/articles/get.articles.js';
-import getArticle from '../src/routes/articles/article/get.article.js';
-import patchArticle from '../src/routes/articles/article/patch.article.js';
-import deleteArticle from '../src/routes/articles/article/delete.article.js';
+import postArticle from "../src/routes/articles/article/post.article.js";
+import getArticles from "../src/routes/articles/get.articles.js";
+import getArticle from "../src/routes/articles/article/get.article.js";
+import patchArticle from "../src/routes/articles/article/patch.article.js";
+import deleteArticle from "../src/routes/articles/article/delete.article.js";
 // Импорт Swagger
-import swaggerRouter from '../src/routes/swagger.js';
+import swaggerRouter from "../src/routes/swagger.js";
 // Импорт роутера для токенов
-import tokenRouter from '../src/routes/tokens/generate.tokens.js';
+import tokenRouter from "../src/routes/tokens/generate.tokens.js";
 const app = new Hono();
 const prisma = new PrismaClient();
 const prismaMidleware = createMiddleware(async (c, next) => {
-    c.set('prisma', prisma);
-    await next();
+  c.set("prisma", prisma);
+  await next();
 });
 app.use(prismaMidleware);
-app.get('/', (c) => {
-    return c.text('AquaWiki API - Добро пожаловать! Документация доступна по адресу: /docs');
+app.get("/", (c) => {
+  return c.text(
+    "AquaWiki API - Добро пожаловать! Документация доступна по адресу: /docs"
+  );
 });
 // Swagger документация
-app.route('/', swaggerRouter);
+app.route("/", swaggerRouter);
 // Генерация токенов
-app.route('/', tokenRouter);
+app.route("/", tokenRouter);
 // Роутеры для категорий
-app.route('/', postCategory);
-app.route('/', getCategories);
-app.route('/', patchCategory);
-app.route('/', deleteCategory);
+app.route("/", postCategory);
+app.route("/", getCategories);
+app.route("/", patchCategory);
+app.route("/", deleteCategory);
 // Роутеры для подкатегорий
-app.route('/', postSubCategory);
-app.route('/', getSubCategories);
-app.route('/', patchSubCategory);
-app.route('/', deleteSubCategory);
+app.route("/", postSubCategory);
+app.route("/", getSubCategories);
+app.route("/", patchSubCategory);
+app.route("/", deleteSubCategory);
 // Роутеры для статей
-app.route('/', postArticle);
-app.route('/', getArticles);
-app.route('/', getArticle);
-app.route('/', patchArticle);
-app.route('/', deleteArticle);
+app.route("/", postArticle);
+app.route("/", getArticles);
+app.route("/", getArticle);
+app.route("/", patchArticle);
+app.route("/", deleteArticle);
 // Отключение Prisma Client при завершении работы приложения
-process.on('beforeExit', async () => {
-    await prisma.$disconnect();
+process.on("beforeExit", async () => {
+  await prisma.$disconnect();
 });
-process.on('SIGINT', async () => {
-    await prisma.$disconnect();
-    process.exit(0);
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
 });
-process.on('SIGTERM', async () => {
-    await prisma.$disconnect();
-    process.exit(0);
+process.on("SIGTERM", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
 });
-// Для Vercel serverless
-export default app;
+
+serve(app);
